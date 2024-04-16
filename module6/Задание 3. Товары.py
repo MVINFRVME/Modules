f28b0f9d3893790f0e8b3feb93609272704ac0ce
MVINFRVME <galgamet1337@yandex.ru>
2024-04-16 23:51:33 +0500

    Добавил практическую работу module 6

---
+++ module6/Задание 3. Товары.py
@@ -0,0 +1,50 @@
+# # Задание 3. Товары
+# Что нужно сделать
+# В базе данных магазина вся необходимая информация по товарам делится на два словаря: первый отвечает за коды товаров,
+# второй — за списки количества разнообразных товаров на складе:
+
+goods = {
+    "Лампа": "12345",
+    "Стол": "23456",
+    "Диван": "34567",
+    "Стул": "45678",
+}
+
+store = {
+    "12345": [{"quantity": 27, "price": 42}],
+    "23456": [{"quantity": 22, "price": 510},
+              {"quantity": 32, "price": 520}],
+    "34567": [{"quantity": 2, "price": 1200},
+              {"quantity": 1, "price": 1150}],
+    "45678": [{"quantity": 50, "price": 100},
+              {"quantity": 12, "price": 95},
+              {"quantity": 43, "price": 97},],
+}
+
+# Каждая запись второго словаря отображает, сколько и по какой цене закупалось товаров. Цена указана за одну штуку.
+# Напишите программу, которая рассчитывает общую стоимость позиций для каждого товара на складе и выводит эту
+# информацию на экран.
+#
+# Результат работы программы:
+# Лампа — 27 штук, стоимость 1134 рубля.
+# Стол — 54 штуки, стоимость 27 860 рублей.
+# Диван — 3 штуки, стоимость 3550 рублей.
+# Стул — 105 штук, стоимость 10 311 рублей.
+
+# for i_key in goods.keys():
+#     total_quantity = 0
+#     total_price = 0
+#     for i_list in range(len(store[goods[i_key]])):
+#         total_quantity += store[goods[i_key]][i_list]['quantity']
+#         total_price += store[goods[i_key]][i_list]['quantity'] * store[goods[i_key]][i_list]['price']
+#     print(f'{i_key} - {total_quantity} шт, стоимость {total_price} рубля')
+
+# или
+
+for name, article in goods.items():
+    total_quantity = 0
+    total_price = 0
+    for i_list in range(len(store[article])):
+        total_quantity += store[article][i_list]['quantity']
+        total_price += store[article][i_list]['price'] * store[article][i_list]['quantity']
+    print(f'{name} - {total_quantity} шт, стоимость {total_price} рубля')
---
+++ module6/Задание 8. Снова палиндром.py
@@ -0,0 +1,52 @@
+# Задание 8. Снова палиндром
+# Что нужно сделать
+# Пользователь вводит строку. Необходимо написать программу, которая определяет, существует ли у этой строки
+# перестановка, при которой она станет палиндромом. Затем она должна выводить соответствующее сообщение.
+#
+# Пример 1.
+# Введите строку: aab
+# Можно сделать палиндромом
+#
+# Пример 2.
+# Введите строку: aabc
+# Нельзя сделать палиндромом
+
+
+text = input('Введите строку: ')
+text_dict = dict()
+
+for sym in text:
+    if sym in text_dict:
+        text_dict[sym] += 1
+    else:
+        text_dict[sym] = 1
+
+odd_count = 0
+for value in text_dict.values():
+    if value % 2 != 0:
+        odd_count += 1
+if odd_count <= 1:
+    print('Можно сделать палиндром')
+else:
+    print('Нельзя сделать палиндром')
+
+# или
+
+# def is_palindrome(my_text):
+#     text_dict = dict()
+#     for sym in my_text:
+#         text_dict[sym] = text_dict.get(sym, 0) + 1
+#
+#     odd_count = 0
+#     for value in text_dict.values():
+#         if value % 2 != 0:
+#             odd_count += 1
+#
+#     return odd_count <= 1
+#
+#
+# text = input('Введите строку: ')
+# if is_palindrome(text):
+#     print('Можно сделать палиндром!')
+# else:
+#     print('Нельзя сделать палиндром.')
