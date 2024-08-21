0d11cdfc04d7781ef586f1cf7b549d7e8edc26c7
MVINFRVME <galgamet1337@yandex.ru>
2024-08-21 15:35:50 +0500

    module 13 fixed

--- module13/Задача 4. Односвязный список.py
+++ module13/Задача 4. Односвязный список.py
@@ -164,6 +164,6 @@ my_list.append(10)
 my_list.append(20)
 my_list.append(30)
 print(my_list)
-my_list.remove(2) # todo так падает с ошибкой
+my_list.remove(1) # todo так падает с ошибкой
 print(my_list)
 print(my_list.get(1))
